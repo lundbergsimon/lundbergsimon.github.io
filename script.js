01b689f29959c4482d75b21f7164c207550299d2
LundbergDev <136829507+lundbergsimon@users.noreply.github.com>
2025-06-03 14:26:54 +0200

    create script.js
    fetches repositories from my GitHub profile, creates a repo-card for each, and append them to repo-list

---
+++ script.js
@@ -0,0 +1,30 @@
+const username = "lundbergsimon";
+
+async function fetchRepositories() {
+  try {
+    const response = await fetch(
+      `https://api.github.com/users/${username}/repos?type=owner&sort=updated&per_page=100`
+    );
+    const repos = await response.json();
+
+    repos.forEach((repo) => {
+      const repositoryCard = document.createElement("a");
+      repositoryCard.href = repo.html_url;
+      repositoryCard.target = "_blank";
+      repositoryCard.innerHTML = `
+          <div class="github-repository-card">
+            <div class="repo-name">${repo.name}</div>
+            <div class="repo-description">${
+              repo.description || "No description"
+            }</div>
+          </div>
+      `;
+      const repositoryCardList = document.getElementById("github-repositories");
+      repositoryCardList.appendChild(repositoryCard);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+fetchRepositories();
